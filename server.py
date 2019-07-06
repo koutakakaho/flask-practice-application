5cc61557decb64ae19957989296f5885c67c3b11
vagrant <vagrant@localhost.localdomain>
2019-07-06 13:01:32 +0100

    add image

--- server.py
+++ server.py
@@ -2,12 +2,12 @@
 from flask import Flask, render_template
 import feedparser
 
-#アプリ
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_name():
-    RSS_URLs = ["https://www.osaka-u.ac.jp/ja/event_rss", "https://www.osaka-u.ac.jp/ja/misc/ja/event_rss?target=all"]
+    RSS_URLs = ["https://www.osaka-u.ac.jp/ja/event_rss", "https://www.osaka-u.ac.jp/ja/misc/ja/event_rss?target=all", "http://www.todainavi.jp/feed/atom/", "https://www.careersupport.adm.u-tokyo.ac.jp/index.php?option=com_jevents&task=modlatest.rss&format=feed&type=rss&modid=139"]
     feed_dics = []
     for RSS_URL in RSS_URLs:
         feed_dic = feedparser.parse(RSS_URL)
