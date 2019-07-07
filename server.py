25729f08d7886e14fda9babab8dd6d67131966c2
vagrant <vagrant@localhost.localdomain>
2019-07-07 12:04:25 +0100

    add submit

--- server.py
+++ server.py
@@ -7,19 +7,31 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        RSS_URLs = ["https://www.osaka-u.ac.jp/ja/event_rss", "https://www.osaka-u.ac.jp/ja/misc/ja/event_rss?target=all", "http://www.todainavi.jp/feed/atom/", "https://www.careersupport.adm.u-tokyo.ac.jp/index.php?option=com_jevents&task=modlatest.rss&format=feed&type=rss&modid=139"]
-        feed_dics = []
-        entry = []
-        for RSS_URL in RSS_URLs:
-            feed_dic = feedparser.parse(RSS_URL)
-            feed_dics.append(feed_dic)
-        for feed_dic in feed_dics:
-            for entries in feed_dic.entries:
-                entry.append(entries)
-        return render_template("index.html", entry= entry)
-    else:
-        return render_template("post.html")
+		RSS_URLs = ["https://www.osaka-u.ac.jp/ja/news_rss"]	
+		entry = []
+		feed_dics = []
+		if request.method == "GET":
+				for RSS_URL in RSS_URLs:
+						feed_dic = feedparser.parse(RSS_URL)
+						feed_dics.append(feed_dic)
+						for feed_dic in feed_dics:
+								for entries in feed_dic.entries:
+										entry.append(entries)
+				return render_template("index.html", entry= entry)
+    
+		else:
+	  #フォームで送信されたRSSのURLをRSS_URLsに付け足す
+		#ここDRYにしたいなぁ
+				result = request.form
+				RSS_URLs.append(result)
+				for RSS_URL in RSS_URLs:
+						feed_dic = feedparser.parse(RSS_URL)
+						feed_dics.append(feed_dic)
+						for feed_dic in feed_dics:
+								for entries in feed_dic.entries:
+										entry.append(entries)
+				return render_template("index.html", entry= entry)
+
 if __name__ == "__main__":
     app.debug = False #本番環境ではFalseにする
     app.run(host="0.0.0.0", port=8888)
