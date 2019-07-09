1a5229143261327258a40a53b355c8b5c5cd8589
vagrant <vagrant@localhost.localdomain>
2019-07-09 03:06:35 +0100

    fixed sort_by_time

--- server.py
+++ server.py
@@ -2,33 +2,60 @@
 from flask import Flask, render_template, url_for, request, redirect
 import feedparser
 import logging
+from datetime import datetime as dt
 
 #アプリケーションの起動
 app = Flask(__name__)
 
+
+def parseDate(dateData):  
+	if dateData == None or dateData == "<":
+		return dt(2010,1,1,0,0,0)
+	elif dateData == "<":
+		return None
+	else:
+		return dt(  
+			dateData.tm_year,  
+			dateData.tm_mon,  
+			dateData.tm_mday,  
+			dateData.tm_hour,  
+			dateData.tm_min,  
+			dateData.tm_sec  
+		)  
+
+#発行者
+def in_key(entry, key):
+	if key in entry:
+		return entry[key]
+	else:
+		return "unknown"
+	
 #表示用のメソッド
 def show(rss_urls):
-		RSS_URLs = rss_urls
-		entry = []
-		feed_dics = []
-		for RSS_URL in RSS_URLs:
-				feed_dic = feedparser.parse(RSS_URL)
-				feed_dics.append(feed_dic)
-				for feed_dic in feed_dics:
-						for entries in feed_dic.entries:
-								entry.append(entries)
-		return entry
+	RSS_URLs = rss_urls
+	entry = []
+	feed_dics = []
+	for RSS_URL in RSS_URLs:
+		feed_dic = feedparser.parse(RSS_URL)
+		for entries in feed_dic.entries:
+			entry.append(entries)
+	# showメソッドの返り値はリスト
+	return entry
 
 #時系列順に並び替える
-def sort_by_time(entry):
-	entry = entry
-	sorted_entry = []
-	for entry in entry:
-		sorted_entry.append(entry)
-	return sorted(sorted_entry, key=lambda x: x["updated"], reverse=True)
+def sort_by_time(entries):
+	entries = [{  
+			'title': entry['title'],  
+			'link': entry['link'],  
+			'date': parseDate(entry['updated_parsed'] or entry['published_parsed']), 
+			"publisher": in_key(entry, "publisher")
+		} 
+			for entry in entries ]
+	print([entry["date"] for entry in entries])
+	return sorted(entries, key=lambda x: x["date"], reverse=True)
 
 #RSS_URLsを宣言
-RSS_URLs = ["https://www.osaka-u.ac.jp/ja/event_rss"]
+RSS_URLs = ["https://www.osaka-u.ac.jp/ja/event_rss","http://www.tuat.ac.jp/event/rss.xml","https://www.titech.ac.jp/alumni/event/rss.xml","https://www.tcu.ac.jp/news/all/feed/"]
 
 #トップページのメソッド
 @app.route("/", methods=["GET"])
